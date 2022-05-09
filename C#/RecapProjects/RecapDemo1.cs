735515ddf26842eceffcb603dd96c486635363d2
DorukBilgi <84389609+DorukBilgi@users.noreply.github.com>
2022-05-10 00:36:54 +0300

    Update RecapDemo1.cs

--- C#/RecapProjects/RecapDemo1.cs
+++ C#/RecapProjects/RecapDemo1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Recap_Demo_1
+namespace Recap_Demo_1 // Chequerboard
 {
     public partial class Form1 : Form //inheritance
     {
